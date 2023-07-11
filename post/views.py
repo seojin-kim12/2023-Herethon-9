f4dc50ad6479f616907db8ce636c47f9edb180ce
Seorish <seojin0774@naver.com>
2023-07-11 22:33:00 +0900

    create 1차 구현

--- post/views.py
+++ post/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import *
 # Create your views here.
+
+def create(request):
+    if request.method == 'POST':
+        post = Post()
+        post.title = request.POST['title']
+        post.body = request.POST['body']
+        post.save()
+        return redirect("create")
+    return render(request, "html/post.html")
