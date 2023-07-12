c0b0c72f2b32040245a45948e0b7f2abdec0c36b
maeng <20221153@sungshin.ac.kr>
2023-07-13 02:42:16 +0900

    feat: 로그인 및 회원가입 기능

--- wofety/settings.py
+++ wofety/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'userinfo',
-    'phonenumber_field'
+    'phonenumber_field',
+    'accounts',
 ]
 
 MIDDLEWARE = [
